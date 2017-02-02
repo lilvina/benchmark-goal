6506e5fa6be4073e9f096888f76f0af3b6275b0d
lilvina <davinaleo2007@yahoo.com>
2017-02-02 14:18:06 -0800

    deleted node modules

---
+++ spec/disemvowling.js
@@ -0,0 +1,13 @@
+// import chai, { expect } from 'chai'
+// import chaiChange from 'chai-change'
+// import {Vowel} from '../src/disemvowling'
+
+// chai.use(chaiChange)
+
+// describe('Vowel', () => {
+//   'use strict'
+
+//   it('exists', () => {
+//     expect(Vowel).to.be.a('function')
+//   })
+// })
