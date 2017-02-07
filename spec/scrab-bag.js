c745f96ac15c687d16c3cbc2c23d3029438dedc9
lilvina <davinaleo2007@yahoo.com>
2017-02-07 10:17:35 -0800

    worked on scrab bag

---
+++ spec/scrab-bag.js
@@ -0,0 +1,45 @@
+import chai, {expect} from 'chai'
+import chaiChange from 'chai-change'
+import ScrabBag from '../src/scrab-bag'
+
+chai.use(chaiChange)
+
+describe('ScrabBag', () => {
+  'use strict'
+
+  it('exists', () => {
+    expect(ScrabBag).to.be.a('function')
+  })
+
+  it('should return the remaining letters.', () => {
+    expect(ScrabBag('abcd')).to.eql({
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3, 
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1,
+      _: 2
+    })
+  })
+})
