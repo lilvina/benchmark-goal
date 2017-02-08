e784fdeb2d9dbc5b65508fec9c70f957f393ce19
lilvina <davinaleo2007@yahoo.com>
2017-02-07 17:29:52 -0800

    editing

--- src/scrab-bag.js
+++ src/scrab-bag.js
@@ -28,7 +28,7 @@ const TILES_TOTALS = {
   _: 2
 };
 
-const exampleOutput = {
+const output = {
   10: ['E'],
   9: ['I'],
   8: ['A'],
@@ -48,7 +48,7 @@ export default function ScrabBag(input) {
     if(usedTilesArray[0] in TILES_TOTALS && TILES_TOTALS.usedTilesArray[i] < 0) {
       TILES_TOTALS.usedTilesArray[i] = TILES_TOTALS.usedTilesArray[i] - 1
     }
-    return exampleOutput
+    return output
   }
 
   let result = {}
@@ -66,46 +66,3 @@ export default function ScrabBag(input) {
 
   return result.join("\n")
 }
-
-// 'use strict'
-
-// export default function ScrabBag(input) {
-//   let scrabbleLetters = {
-//     A: 9,
-//     B: 2,
-//     C: 2,
-//     D: 4,
-//     E: 12,
-//     F: 2,
-//     G: 3, 
-//     H: 2,
-//     I: 9,
-//     J: 1,
-//     K: 1,
-//     L: 4,
-//     M: 2,
-//     N: 6,
-//     O: 8,
-//     P: 2,
-//     Q: 1,
-//     R: 6,
-//     S: 4,
-//     T: 6,
-//     U: 4,
-//     V: 2,
-//     W: 2,
-//     X: 1,
-//     Y: 2,
-//     Z: 1,
-//     _: 2
-//   }
-
-//   let array = input.toUpperCase().split(' ')
-//   for(let i = 0; i < array.length; i++) {
-//     let currentLetters = array[i]
-//     let remainderPieces = scrabbleLetters[currentLetters]
-//     remainderPieces = remainderPieces - 1
-//     scrabbleLetters = remainderPieces[currentLetters]
-//   }
-//   return scrabbleLetters
-// }
