93e2ac8f38191f15101579405ab5299f3b9c5c52
lilvina <davinaleo2007@yahoo.com>
2017-02-08 17:59:04 -0800

    working on JSON

---
+++ spec/JSON.js
@@ -0,0 +1,34 @@
+import chai, {expect} from 'chai'
+import findPath from '../src/JSON'
+
+describe.only('findPath', () => {
+  it('exists', () => {
+    expect(findPath).to.be.a('function')
+  })
+
+  it('should return specific path of the JSON.', () => {
+
+    const data = {
+      "name": "William Shakespeare",
+      "dead": true,
+      "works": [
+        {
+          "name": "Romeo and Juliet",
+          "published": 1951,
+          "isAwesome": true
+        },
+        {
+          "name": "Richard III",
+          "published": 1952,
+          "isAwesome": false
+        }
+      ],
+      "favoriteSites": [
+        "tumblr",
+        "4chan"
+      ]
+    }
+
+    console.log(findPath(data))
+  })
+})
---
+++ src/JSON.js
@@ -0,0 +1,23 @@
+
+export default function findPath(obj, value) {
+
+  let result = []
+
+  console.log('value', value)
+  console.log('obj', obj)
+
+  for(let key in obj) {
+    console.log('======>', key)
+    if(obj[key] == value) {
+      result = `${key} -> `
+      result.push(value)
+    } else {
+      result.push(value)
+    }
+  }
+
+  console.log('results =======>', result)
+
+  return result
+}
+
